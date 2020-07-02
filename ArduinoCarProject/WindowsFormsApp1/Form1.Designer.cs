564340617453724d84566b72515269599089ce8b
Georgi1102 <gogopro27@abv.bg>
2020-07-03 01:41:55 +0300

    FirstCommit

---
+++ ArduinoCarProject/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PortBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PortBox
+            // 
+            this.PortBox.FormattingEnabled = true;
+            this.PortBox.Location = new System.Drawing.Point(210, 120);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(366, 24);
+            this.PortBox.TabIndex = 2;
+            this.PortBox.SelectedIndexChanged += new System.EventHandler(this.PortBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 116);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Control Panel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PortBox);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox PortBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ArduinoCarProject/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+       // private SerialPort myport;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitializePortConnection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                PortBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+                serialPort1.Open();
+                PortBox.SelectedIndex = 0;
+                serialPort1.BaudRate = (9600);
+                serialPort1.ReadTimeout = (2000);
+                serialPort1.WriteTimeout = (2000);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //private void OnBtn_Click(object sender, EventArgs e)
+        //{
+        //    
+        //}
+
+        //private void OffBtn_Click(object sender, EventArgs e)
+        //{
+           
+        //}
+
+        private void PortBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = PortBox.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.B)
+            {
+                // MessageBox.Show("Off should be triggered!");
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.WriteLine("back");
+                    Console.WriteLine("back");
+                    serialPort1.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            // the diode should be on
+            if (e.KeyCode == Keys.O)
+            {
+                // MessageBox.Show("On should be triggered!");
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.WriteLine("on");
+                    Console.WriteLine("on");
+                    serialPort1.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            //the diode should be off
+            if (e.KeyCode == Keys.F)
+            {
+                // MessageBox.Show("Off should be triggered!");
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.WriteLine("off");
+                    Console.WriteLine("off");
+                    serialPort1.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+
+
+        }
+
+        //private void InitializePortConnection()
+        //{
+        //    myport = new SerialPort();
+        //    myport.BaudRate = 9600;
+        //    myport.
+        //}
+    }
+}
