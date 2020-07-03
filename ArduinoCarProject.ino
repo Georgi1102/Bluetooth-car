2d51c80353b10503f09c0adb302597301457ddf1
Georgi1102 <gogopro27@abv.bg>
2020-07-04 00:23:55 +0300

    SecondCommit
    Added control of the dc motors and forward and backward movement of the car

---
+++ ArduinoCarProject.ino
@@ -0,0 +1,96 @@
+#include <AFMotor.h>
+
+//all motors
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+//debug pin
+#define TEST_PIN 13
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+  
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
+
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{    
+   String readString;
+   String data;
+   
+  while(Serial.available())
+  {
+    delay(1);
+    if(Serial.available()>0)
+    {
+      char inputData = Serial.read();
+      if(isControl(inputData))
+      {
+        break;
+      }
+      readString += inputData;
+    }
+  }
+    
+   data = readString;
+
+    if(data == "stop")
+    {
+       StopAllMotors();
+    }
+
+   if(data == "forward")
+   {
+       ForwardMovement();
+   }
+   
+   if(data == "backward")
+   {
+       BackwardMovement();
+   }
+  
+}
+
+
+void StopAllMotors(void)
+{   
+   motor1.run(RELEASE);
+   motor2.run(RELEASE);
+   motor3.run(RELEASE);
+   motor4.run(RELEASE);
+        
+   Serial.println("stop");
+        
+   digitalWrite(13, LOW);
+}
+
+
+void ForwardMovement(void)
+{
+   digitalWrite(13, HIGH);
+   motor1.run(FORWARD);
+   motor2.run(FORWARD);
+   motor3.run(BACKWARD);
+   motor4.run(BACKWARD);
+
+   Serial.println("forward");
+}
+
+void BackwardMovement(void)
+{
+   motor1.run(BACKWARD);
+   motor2.run(BACKWARD);
+   motor3.run(FORWARD);
+   motor4.run(FORWARD);
+
+   Serial.println("backward");
+}
